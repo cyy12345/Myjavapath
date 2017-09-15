0bff1cffcfafe5dffb20d073d57b2b9b636b7645
cyy <892471766@qq.com>
2017-09-14 22:12:46 -0700

    排序

--- MyFirstProject/src/cyy/demo/Demo1.java
+++ MyFirstProject/src/cyy/demo/Demo1.java
@@ -1,11 +1,55 @@
 package cyy.demo;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class Demo1 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        //
         Dog dogs[]=new Dog[4];
 
-        dogs[0].setName("huahua");
-        dogs[0].setWeight(3.3f);
+        /*dogs[0].setName("huahua");
+        dogs[0].setWeight(3.3f);*/
+        InputStreamReader isr=new InputStreamReader(System.in);
+        BufferedReader br=new BufferedReader(isr);
+
+        for(int i=0;i<4;i++)
+        {
+            //chong
+            dogs[i]=new Dog();
+            System.out.println("Please input dog name:");
+            //Read dogs name from console
+            String name =br.readLine();
+            //Give the name to the dog
+            dogs[i].setName(name);
+            System.out.println("Please input the weight of the dog");
+            String s_weight=br.readLine();
+            float weight=Float.parseFloat(s_weight);
+            dogs[i].setWeight(weight);
+        }
+        //calculate the sum of weight
+        float allWeight=0;
+        for(int i=0;i<4;i++)
+        {
+            allWeight+=dogs[i].getWeight();
+        }
+        //calculate the average weight
+        float avgWeight=allWeight/dogs.length;
+        System.out.println("the sum of weight:"+allWeight+" average weight"+avgWeight);
+        float maxWeight=dogs[0].getWeight();
+        int maxIndex=0;
+        for (int i=1;i<dogs.length;i++)
+        {
+            if(maxWeight<dogs[i].getWeight())
+            {
+                maxWeight=dogs[i].getWeight();
+                maxIndex=i;
+            }
+        }
+        System.out.println(maxWeight);
+
+        //zi fu chuan nei rong shi fou xiang deng yong  equals bu yao yong ==
     }
 }
 
@@ -23,6 +67,8 @@ class Dog
     }
     public void setWeight(float weight){
         this.weight=weight;
-
+    }
+    public float getWeight(){
+        return this.weight;
     }
 }
---
+++ MyFirstProject/src/cyy/test/Demo2.java
@@ -0,0 +1,103 @@
+/*
+  演示各种排序方法
+ */
+
+package cyy.test;
+
+import java.util.Calendar;
+
+public class Demo2 {
+    public static void main(String[] args){
+        int len=100000;
+        int[] arr1=new int[len];
+        for(int i=0;i<len;i++)
+        {
+            //让程序随机产生一个1-10000的数
+            //Math.random()会随机产生0-1的数
+            int t=(int)(Math.random()*10000);
+        }
+
+        Bubble bubble=new Bubble();
+        Calendar cal= Calendar.getInstance();
+        System.out.println(cal.getTime());
+        bubble.sort(arr1);
+        cal=Calendar.getInstance();
+        System.out.println(cal.getTime());
+        //简单数据类型不会变化
+        //int a=12;
+        //bubble.test(a);
+        //System.out.println(a);
+
+        //Select select=new Select();
+        //在排序前打印系统时间
+        //单态模式,开发者模式,23种模式
+        //Calendar cal= Calendar.getInstance();
+        //System.out.println(cal.getTime());
+        //select.sort(arr1);
+        //重新得到实例
+        //cal=Calendar.getInstance();
+        //System.out.println(cal.getTime());
+    }
+}
+//冒泡排序
+//两两比较,小的放前,大的放后,一次內循环可以确定出一个最大的在数组末尾
+class Bubble{
+
+    //测试函数
+    public void test(int a){
+        a++;
+    }
+    public void sort(int arr[]) {
+        int temp = 0;
+        //外循环
+        for (int i = 0; i < arr.length - 1; i++) {
+            //內循环
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+        /*for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }*/
+    }
+}
+
+//选择排序法
+//由第一个开始和后面所有的元素进行比较,选出最小的放到第一个,
+//然后是第二个与其后面所有的进行比较,以此类推
+class Select
+{
+    public void sort(int arr[])
+    {
+        int temp=0;
+
+        for(int j=0;j<arr.length-1;j++) {
+            //我认为第一个数就是最小的
+            int min = arr[j];
+            //记录最小数的下标
+            int minIndex = j;
+
+            for (int k = j + 1; k < arr.length; k++) {
+                if (min > arr[k])
+                //修改最小值
+                {
+                    min = arr[k];
+                    minIndex = k;
+                }
+            }
+
+            //当推出for就找到这次的最小值
+            temp = arr[j];
+            arr[j] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        /*for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }*/
+    }
+}
